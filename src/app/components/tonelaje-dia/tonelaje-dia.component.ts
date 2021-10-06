55b2b2e0fb91447ea04c8fa0fd43f4cf0fd38f39
LePhew <marcos_eduardo16@hotmail.com>
2021-10-06 00:13:26 -0400

    removed things that weren't needed.

--- src/app/components/tonelaje-dia/tonelaje-dia.component.ts
+++ src/app/components/tonelaje-dia/tonelaje-dia.component.ts
@@ -4,8 +4,6 @@ import { ExcelService } from 'src/app/services/excel.service';
 import { GenericService } from 'src/app/services/generic.service';
 import { DatePipe } from '@angular/common';
 
-import { csvObj } from 'src/app/interfaces/csvObj';
-
 @Component({
   selector: 'app-tonelaje-dia',
   templateUrl: './tonelaje-dia.component.html',
@@ -27,26 +25,6 @@ export class TonelajeDiaComponent implements OnInit {
 
   ngOnInit(): void {
     this.getPesaje();
-    this.users = [
-      {
-        id: 1,
-        firstName: 'Mark',
-        lastName: 'Otto',
-        handle: '@mdo'
-      },
-      {
-        id: 2,
-        firstName: 'Jacob',
-        lastName: 'Thornton',
-        handle: '@fat'
-      },
-      {
-        id: 3,
-        firstName: 'Larry',
-        lastName: 'the Bird',
-        handle: '@twitter'
-      },
-    ];
   }
 
   getPesaje() {
